f9b3fe0ec5b6d633a50bc49ef16cde4225718ff3
Rémy R <remy.rabischung@laplateforme.io>
2024-03-25 15:59:11 +0100

    Job 9 commencé mais non terminé

---
+++ Jour01/Job09/script.js
@@ -0,0 +1,15 @@
+function tri(numbers){
+    let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+    if (order = asc) {
+        console.log(numbers.sort())
+    } else {
+        return false
+    }
+    if (order = desc) {
+        console.log(numbers.reverse())
+    } else {
+        return false
+    }
+}
+order = asc
+tri(numbers)
