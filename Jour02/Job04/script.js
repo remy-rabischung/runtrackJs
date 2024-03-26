f742b68cf9d575a22ffb09ca18a68db12cee638f
Rémy R <remy.rabischung@laplateforme.io>
2024-03-26 14:30:48 +0100

    Ajustement du code pour le Job 4 : terminé

--- Jour02/Job04/script.js
+++ Jour02/Job04/script.js
@@ -1,26 +1,16 @@
 var textarea = document.getElementById("keylogger");
-var isTextareaFocused = false;
-
-textarea.addEventListener("focus", function() {
-    isTextareaFocused = true;
-});
-
-textarea.addEventListener("blur", function() {
-    isTextareaFocused = false;
-});
 
 document.addEventListener("keydown", function(event) {
     var keyPressed = event.key.toLowerCase();
-    
     if (/^[a-z]$/.test(keyPressed)) {
-        event.preventDefault(); 
-
-        if (isTextareaFocused) {
+        event.preventDefault();
+        if (document.activeElement === textarea) {
             textarea.value += keyPressed + keyPressed;
         } else {
             textarea.value += keyPressed;
         }
     }
 });
+
 
 
