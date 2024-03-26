572c12dd28a6c5d8731b2301bf60c6211b4ce443
Rémy R <remy.rabischung@laplateforme.io>
2024-03-26 09:43:09 +0100

    Job 1 terminé

---
+++ Jour02/Job01/script.js
@@ -0,0 +1,6 @@
+document.getElementById("button").addEventListener("click", citation);
+
+function citation() {
+    let citationText = document.getElementById("citation").textContent;
+    console.log(citationText);
+}
